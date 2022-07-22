34e5f09d181cdae9ee099896a57f5d6374daf46b
amonint <payala909@gmail.com>
2022-07-22 11:38:49 -0500

    AAA

---
+++ EjercicioJUnit/test/paquete1/EstudiantePresencialTest.java
@@ -0,0 +1,35 @@
+package paquete1;
+
+import org.junit.Test;
+
+public class EstudiantePresencialTest {
+    @Test
+    public void testCalcularMatriculaPresencial() {
+
+    }
+
+    @Test
+    public void testEstablecerCostoCredito() {
+
+    }
+
+    @Test
+    public void testEstablecerNumeroCreditos() {
+
+    }
+
+    @Test
+    public void testObtenerCostoCredito() {
+
+    }
+
+    @Test
+    public void testObtenerMatriculaPresencial() {
+
+    }
+
+    @Test
+    public void testObtenerNumeroCreditos() {
+
+    }
+}
